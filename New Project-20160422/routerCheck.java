d12d818dbccdb36043d2282d2b2b580f4aefcf34
kovacs-peter <kovacs-peter@users.noreply.github.com>
2016-04-22 10:40:39 +0200

    Created by Coding Ground Client

---
+++ New Project-20160422/routerCheck.java
@@ -0,0 +1,16 @@
+public class routerCheck{
+
+     public static void main(String []args){
+//paraméter beolvasás: delay, url, timeout, event_till_reconnect, event_till_reboot, reconnect_cmd, reboot_cmd
+    int delay=2000;
+    char[] server="index.hu"
+    timeout=1000;
+    event_till_reconnect=3;
+    
+    InetAddress.isReachable(timeout);
+
+        System.out.println("Hello World");
+
+
+     }
+}
